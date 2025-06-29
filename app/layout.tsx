19b5fab8de84de63ed7c8d401fbbca47dad3c848
griffitystudios <griffitystudios@gmail.com>
2025-06-30 00:03:14 +0545

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "../globals.css";
 import { ReactNode } from "react";
 import { poppins } from "@/fonts";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -57,6 +58,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body
         className={`bg-body ${poppins.className} text-white transition-all ease-in-out duration-700`}
       >
+            <Analytics />
         <main>{children}</main>
       </body>
     </html>
