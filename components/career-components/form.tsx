597b58851d28f733f017b363a77c0634fa252ecd
Susan Shrestha <shresthasusan44@gmail.com>
2025-08-31 10:37:10 +0545

    typo

--- components/career-components/form.tsx
+++ components/career-components/form.tsx
@@ -170,7 +170,7 @@ const JobForm = () => {
           <p className=" text-[1rem] font-light text-white mt-4">
             Accepted Formats:{" "}
             <span className="text-primary">.pdf, .doc, .docx. </span> Send mail
-            at hr@griffitystudios.com for any query.
+            at hr@griffitystudios.com for any inquiries.
           </p>
         </div>
         <button
